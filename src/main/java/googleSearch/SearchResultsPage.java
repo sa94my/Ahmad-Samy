52405373204c54f0a9c4bdd2d4af0c31c67209f8
sa94my <ahmad_samy93@yahoo.com>
2021-11-21 18:41:46 +0200

    finalize searchResults tests

--- src/main/java/googleSearch/SearchResultsPage.java
+++ src/main/java/googleSearch/SearchResultsPage.java
@@ -4,6 +4,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.AppiumFluentWait;
 import io.appium.java_client.functions.ExpectedCondition;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,15 +12,44 @@ import java.time.Duration;
 
 public class SearchResultsPage {
     private AppiumDriver driver;
+    private By searchFieldLocator= By.name("q");
     private By imageTab=By.xpath("//div[@class='hdtb-mitem' and .='صور']");
     private By videoTab=By.xpath("//div[@class='hdtb-mitem' and .='فيديو']");
     private By genericLocator = By.cssSelector("div.hdtb-mitem");
+    private String searchResultsContainers = "(//div[@class='tF2Cxc'][.//div[contains(@class,'VwiC3b ')]])";
+
     WebDriverWait wait ;
 
     public SearchResultsPage(AppiumDriver driver,String keyword) {
         this.driver = driver;
         driver.get("https://www.google.com/search?q=instabug");
     }
+    public void searchWithKey(String keyword){
+        driver.findElement(searchFieldLocator).clear();
+        driver.findElement(searchFieldLocator).sendKeys(keyword);
+        driver.findElement(searchFieldLocator).sendKeys(Keys.ENTER);
+    }
+
+    public boolean verifyUserisRedirectedToSearchResults(String matcher){
+        return driver.getCurrentUrl().contains("/search?q="+matcher.replace(" ","+"));
+    }
+    // get the textual content of a given search result
+    public String getNthResultText(int resultIndex){
+        return driver.findElement(getXpathForNthSearchResult(resultIndex)).getText();
+    }
+    /*
+    for internal use
+    to allow for dynamic selection of search results
+     */
+    private By getXpathForNthSearchResult(int resultIndex){
+        /*
+            TODO
+            error handling if input exceeds number of search results
+         */
+        String locatorToSpecificSearchResult = searchResultsContainers+"["+resultIndex+"]";
+        return By.xpath(locatorToSpecificSearchResult);
+    }
+
     public boolean checkAllTabsExist(){
     return checkImageTab() && checkVideoTab();
     }
--- src/test/java/search/SearchResultsTests.java
+++ src/test/java/search/SearchResultsTests.java
@@ -5,10 +5,27 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SearchResultsTests extends TestBase {
-
+    SearchResultsPage searchResultsPage;
+    String secondSearchKeyword = "shake to report";
+    String searchKeyword="instabug";
+    String relevanceSearchKeyword ="unicorn" ;
+    int resultIndex = 1;
     @Test
     public void checkForTabs(){
-        SearchResultsPage searchResultsPage = new SearchResultsPage(driver,"instabug");
+         searchResultsPage = new SearchResultsPage(driver,searchKeyword);
         Assert.assertTrue(searchResultsPage.checkAllTabsExist());
     }
+    @Test
+    public void testSearchFunction(){
+
+        searchResultsPage.searchWithKey(secondSearchKeyword);
+        Assert.assertTrue(searchResultsPage.verifyUserisRedirectedToSearchResults(secondSearchKeyword));
+
+    }
+//    @Test
+//    public void checkRelevanceOfNthResult(){
+//        searchResultsPage.searchWithKey(relevanceSearchKeyword);
+//        searchResultsPage.getNthResultText(1);
+//    }
+
 }
